45a4a7d4cead5224c178b76a48a1970d6789feef
skinshy <yyshgk12@gmail.com>
2023-03-02 15:01:39 +0900

    fix utils folder name

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Layout from '../../components/layout';
 import Date from '../../components/date';
-import { getAllPostIds, getPostData } from '../../lib/posts';
+import { getAllPostIds, getPostData } from '../../utils/posts';
 import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
